40a9a47747499d86202ba309192ea06050a84926
Ifeoluwa <ifeolufaro@gmail.com>
2023-04-13 12:26:35 +0100

    second commit

--- detect.py
+++ detect.py
@@ -5,7 +5,6 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 
 
-
 mask_model = load_model('model/kaggle/working/mask_model')
 
 f_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -17,35 +16,22 @@ class MaskDetector():
          pass
     
     def detect_mask(self):
+        
+#         img_file = path_to_img_file
 
         cam_capture = cv.VideoCapture(0)
         
         while True:
             ret, frame = cam_capture.read()
 
-            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            faces = MaskDetector.detect_faces(frame)
 
-            faces = f_cascade.detectMultiScale(gray, 1.1, 5)
-
-
-            for x, y, w, h in faces:
+            for face in faces:
+                x,y,w,h = face
                 roi = frame[y:y+h, x:x+w]
 #                 cv.imshow('ROI', roi)
                 mask_prob = MaskDetector.predict_image_with_mask(roi, mask_model) 
-                color = None
-                text = None
-                disp_prob = 0
-                if mask_prob >= 0.5:
-                    color = (0,255,0)
-                    text = 'Mask'
-                    disp_prob = mask_prob
-                else:
-                    color = (0,0,255)
-                    text = 'No Mask'
-                    disp_prob = 1 - mask_prob
-                
-                cv.rectangle(frame, (x,y), (x+w, y+h), color, thickness=1)
-                cv.putText(roi, f'{text} {round(disp_prob*100, 2)}%', (5, roi.shape[0]//2), cv.FONT_HERSHEY_SIMPLEX, fontScale=0.35, color=color, thickness=1)
+                frame = MaskDetector.draw_bounding_box(frame, face, mask_prob)
         
             cv.imshow('Face', frame)
             
@@ -54,7 +40,6 @@ class MaskDetector():
                 
         cam_capture.release()
         cv.destroyAllWindows()
- 
             
     def preprocess_image(img):
         img_arr = img_to_array(img)
@@ -67,8 +52,32 @@ class MaskDetector():
         probability = 1 - model.predict(img_arr).flatten()[0]
         print(f"probablity of mask present: {probability:.04}")
         return probability
+    
+    def detect_faces(frame):
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faces = f_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8)
+        return faces
+    
+    def draw_bounding_box(frame, face, mask_prob):
+        color = None
+        text = None
+        disp_prob = 0
+
+        (x,y,w,h) = face
+        if mask_prob >= 0.5:
+            color = (0,255,0)
+            text = 'Mask'
+            disp_prob = mask_prob
+        else:
+            color = (0,0,255)
+            text = 'No Mask'
+            disp_prob = 1 - mask_prob
+
+        cv.rectangle(frame, (x,y), (x+w, y+h), color, thickness=1)
+        cv.putText(frame, f'{text} {round(disp_prob*100, 2)}%', (x, y-10), cv.FONT_HERSHEY_SIMPLEX, fontScale=0.35, color=color, thickness=1, lineType=cv.LINE_AA, bottomLeftOrigin=False)
+        return frame
      
         
 if __name__ == '__main__':
     mask_detector = MaskDetector()
-    mask_detector.detect_mask()   
+    mask_detector.detect_mask()
